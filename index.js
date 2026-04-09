5e27f7dc4cb39d908be07f0221bd76487a6dd34a
Darking053 <darking053@protonmail.com>
2026-04-10 01:46:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const { Client, GatewayIntentBits } = require("@jubbio/core");
 
+// Tüm intent'leri dene
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: 3276799 // Tüm intent'lerin bit mask'i
 });
 
-client.on("ready", () => console.log("Bot hazır!"));
+client.on("ready", () => {
+  console.log("Bot hazır!");
+  console.log("Bot ID:", client.user?.id);
+});
 
 client.on("messageCreate", (msg) => {
-  console.log("GELEN MESAJ:", msg.content);
+  console.log("MESAJ:", msg.content);
+  if (msg.content === "!ping") {
+    msg.reply("Pong!").catch(console.error);
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
